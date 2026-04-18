67ca8e0050c55b32dc6fd5bad14c7c1dbb36dd6e
anantbarjatya <barjatyaanant23@gmail.com>
2026-04-18 14:42:12 +0530

    Secured Firebase config using env variables

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyBYe0R8cxR9J1oZyuBiRjn4MumAWqZKrE4",
-  authDomain:        "tala-639b6.firebaseapp.com",
-  projectId:         "tala-639b6",
-  storageBucket:     "tala-639b6.firebasestorage.app",
-  messagingSenderId: "790267447287",
-  appId:             "1:790267447287:web:5eed7a1e436474175510f2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-const app      = initializeApp(firebaseConfig);
-export const auth     = getAuth(app);
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
