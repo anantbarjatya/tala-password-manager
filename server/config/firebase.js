fd3b2f54781247278695712e691ac49c04d464b1
anantbarjatya <barjatyaanant23@gmail.com>
2026-04-18 15:29:37 +0530

    Use Firebase service account from env

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,8 +1,6 @@
 import admin from 'firebase-admin';
-import { createRequire } from 'module';
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require('../firebase-service-account.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
